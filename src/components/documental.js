f34643d487fc2aa4e74a1fb6c685f0654970e859
JP Baez <baezjuanpablo@gmail.com>
2021-09-14 00:45:24 +0200

    new docu and sound page

--- src/components/documental.js
+++ src/components/documental.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, {  useEffect, useState } from "react";
 import ReactPlayer from 'react-player';
 
 
 const Documental = ({documental, video}) => {
+    const [videoList, setVideoList] = useState([]);
+
+    useEffect(() =>{
+        setVideoList(documental);
+        return
+    },[])
+
+
+   function handleDoculist () {
+       setVideoList(documental)
+   }
+   function handleVideolist () {
+        setVideoList(video)
+   }
+    
+    console.log(videoList)
     return(
         <div className="documental-body">
-            <h2>Documental</h2>
-            <h4 className="patrocinio">El catalogo online completo disponible en beatkino.com</h4>
+
+            <div className="buttons">
+                <button onClick={handleDoculist} >DOCUMENTAL</button>/
+                <button onClick={handleVideolist}>VIDEO</button>/
+                <button>MUSICA</button>/
+                <button>TALLERES</button>     
+            </div>
+            
+            { videoList===documental &&  
+                <p className="patrocinio">El catalogo online completo disponible en <a href="http://beatkino.com/" rel="noreferrer" target="_blank">beatkino.com</a></p>
+            }          
             {
-                documental.map((item, index) => {
+                videoList.map((item, index) => {
                     if (item.fields.link) {
                         return(
                             <div className="video-container" key={index}>
@@ -28,38 +53,7 @@ const Documental = ({documental, video}) => {
                 }) 
             }
 
-            <h2>Video</h2>
-            {
-                video.map((item, index) => {
-                    if (item.fields.link) {
-                        return(
-                            <div className="video-container" key={index}>
-                                <ReactPlayer 
-                                className="page-video"
-                                url={item.fields.link}
-                                controls={true}
-                                responsive="true"
-                                width="100%"
-                                height="50vh"
-                                config={{ 
-                                    vimeo: {
-                                        playerOptions:{
-                                            height: "100%",
-                                            width:"100%",
-                                            responsive: "false",
-                                            
-                                            
-                                        }
-                                    }
-                                }}
-                                    /> 
-                            <h3>{item.fields.title}</h3>
-                            <p>{item.fields.description}</p>
-                            </div>
-                        )
-                    } return null
-            }) 
-            } 
+           
         </div>
         
     )
--- src/components/sonoro.js
+++ src/components/sonoro.js
@@ -7,10 +7,61 @@ const Sonoro = () => {
         <div className="sonoro-body">
         <ReactPlayer
             className="sonoro-player"
-            url="https://soundcloud.com/carloslertora"
+            url="https://soundcloud.com/carloslertora/ojos-de-agua"
             responsive="true"
             width="80%"
-            height="60vh"
+            height="15vh"
+            config={{
+                soundcloud: {
+                    options:{
+                    download: false,
+                    sharing: true,
+                    show_artwork: false,
+                    show_user: false,
+                    color: "##36ff17"}
+                }
+            }}
+        />
+        <ReactPlayer
+            className="sonoro-player"
+            url="https://soundcloud.com/carloslertora/estados-de-la-materia"
+            responsive="true"
+            width="80%"
+            height="15vh"
+            config={{
+                soundcloud: {
+                    options:{
+                    download: false,
+                    sharing: true,
+                    show_artwork: false,
+                    show_user: false,
+                    color: "##36ff17"}
+                }
+            }}
+        />
+        <ReactPlayer
+            className="sonoro-player"
+            url="https://soundcloud.com/carloslertora/sonadores-cosmicos-yerko-andrade"
+            responsive="true"
+            width="80%"
+            height="15vh"
+            config={{
+                soundcloud: {
+                    options:{
+                    download: false,
+                    sharing: true,
+                    show_artwork: false,
+                    show_user: false,
+                    color: "##36ff17"}
+                }
+            }}
+        />
+        <ReactPlayer
+            className="sonoro-player"
+            url="https://soundcloud.com/carloslertora/el-retorno-a-la-aldea"
+            responsive="true"
+            width="80%"
+            height="15vh"
             config={{
                 soundcloud: {
                     options:{
