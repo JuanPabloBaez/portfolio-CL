066d27f52a28fc8afcfa3e605bb40a1521f273fb
JP Baez <baezjuanpablo@gmail.com>
2021-10-03 10:36:10 +0200

    analytics + typo

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 const Nav = () => {
     return(
         <div className="nav-bar">    
-            <Link to="/" className="logo">CARLOS LERTORA SÁEZ</Link>
+            <Link to="/" className="logo">CARLOS LÉRTORA SÁEZ</Link>
             <div className="link-panel">
                 <Link to="/documental">DOCUMENTAL & VIDEO </Link>/
                 <Link to="/imagen">IMAGEN </Link>/
